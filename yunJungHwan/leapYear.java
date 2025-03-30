f6fa83da28a3314ea1bf33abddbb9ed637de5a7f
yun <yuntyu01@gmail.com>
2025-03-30 13:32:58 +0900

    4주차 동아리 과제

---
+++ yunJungHwan/leapYear.java
@@ -0,0 +1,23 @@
+package yunJungHwan;
+
+import java.io.*;
+
+public class leapYear {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int a = Integer.parseInt(br.readLine());
+
+        if (a % 4 == 0 && a % 100 != 0) {
+            bw.write("1");
+        } else if (a % 400 == 0) {
+            bw.write("1");
+        } else  {
+            bw.write("0");
+        }
+
+        bw.flush();
+
+    }
+}
